626457427f7ea006f495eeccc4ac9324f92917f2
Agustín Vega <agulv2002@gmail.com>
2021-06-29 20:19:23 -0300

    Agregadas funciones de verificación de inicio de sesión

--- config/funciones.php
+++ config/funciones.php
@@ -7,3 +7,49 @@ function debug($value) {
     echo "</pre>";
     exit;
 }
+
+function isAuth_admin() {
+    session_start();
+
+    // Caso de que no haya iniciado sesion aún
+    if(empty($_SESSION)) {
+        header('Location: ../index.html');
+    } 
+     
+     // Si inició sesion, verificamos que haya sido como admin
+     if(!$_SESSION['sesion_admin']) {
+        header('Location: ../index.html') ?? header('Location: ../../index.html');
+     } 
+     
+} 
+
+function isAuth_alumno() {
+    session_start();
+
+    // Caso de que no haya iniciado sesion aún
+    if(empty($_SESSION)) {
+        header('Location: ../index.html');
+    } 
+     
+     // Si inició sesion, verificamos que haya sido como admin
+     if(!$_SESSION['sesion_alumno']) {
+        header('Location: ../index.html') ?? header('Location: ../../index.html');
+     } 
+     
+} 
+
+
+function isAuth_docente() {
+    session_start();
+
+    // Caso de que no haya iniciado sesion aún
+    if(empty($_SESSION)) {
+        header('Location: ../index.html');
+    } 
+     
+     // Si inició sesion, verificamos que haya sido como admin
+     if(!$_SESSION['sesion_docente']) {
+        header('Location: ../index.html') ?? header('Location: ../../index.html');
+     } 
+     
+} 
