cfa39b545c8046cae8e0e36ea0f356603a57ebf6
Agustín Vega <agulv2002@gmail.com>
2021-07-01 22:50:01 -0300

    Estilos y archivo que alterna entre consultas actualizado

--- build/js/consultas.js
+++ build/js/consultas.js
@@ -1,10 +1,36 @@
 // Botones
 const pendiente = document.querySelector('#pendiente')
 const contestada = document.querySelector('#contestada');
+const realizada = document.querySelector('#realizada');
 
-// Contenedores *
+// Contenedores 
+const realizadaContainer = document.querySelector('#realizada-container');
 const pendienteContainer = document.querySelector('#pendiente-container')
 const contestadaContainer = document.querySelector('#contestada-container');
+
+
+if(realizada) {
+    realizada.addEventListener('click', () => {
+        // Boton
+        realizada.classList.toggle('btn-realizadas--active');
+    
+        // Contenedores
+        realizadaContainer.classList.remove('display-none');
+        pendienteContainer.classList.add('display-none')
+        contestadaContainer.classList.add('display-none');
+    
+    
+        // Elimino el active (si lo tiene) 
+        if(contestada.classList.contains('btn-contestadas--active')) {
+            contestada.classList.remove('btn-contestadas--active');
+        }
+    
+        if(pendiente.classList.contains('btn-pendientes--active')) {
+            pendiente.classList.remove('btn-pendientes--active');
+        }
+    })
+}
+
 
 pendiente.addEventListener('click', () => {
     // Boton
@@ -14,10 +40,21 @@ pendiente.addEventListener('click', () => {
     pendienteContainer.classList.remove('display-none')
     contestadaContainer.classList.add('display-none')
 
+    if(realizada) {
+        realizadaContainer.classList.add('display-none')    
 
+        // Quita la clase activa (si la tiene)
+        if(realizada.classList.contains('btn-realizadas--active')) {
+            realizada.classList.remove('btn-realizadas--active');
+        }
+    }
+
+
+    // Elimino el active (si lo tiene) 
     if(contestada.classList.contains('btn-contestadas--active')) {
         contestada.classList.remove('btn-contestadas--active');
     }
+
 })
 
 contestada.addEventListener('click', () => {
@@ -27,8 +64,20 @@ contestada.addEventListener('click', () => {
     // Contenedores
     contestadaContainer.classList.remove('display-none')
     pendienteContainer.classList.add('display-none')
+    
+    if(realizada) {
+        realizadaContainer.classList.add('display-none')    
 
+         // Quita la clase activa (si la tiene)
+        if(realizada.classList.contains('btn-realizadas--active')) {
+            realizada.classList.remove('btn-realizadas--active');
+        }
+    }
+
+    // Elimino el active (si lo tiene) 
     if(pendiente.classList.contains('btn-pendientes--active')) {
         pendiente.classList.remove('btn-pendientes--active');
     }
+
+    
 })
